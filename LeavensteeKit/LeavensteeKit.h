fad299b834c508cf5059c2bdc9a8cd0e8b4fac9a
Steven Lee <stevenleedos@gmail.com>
2022-01-03 11:37:04 -0600

    Initial Commit

---
+++ LeavensteeKit/LeavensteeKit.h
@@ -0,0 +1,18 @@
+//
+//  LeavensteeKit.h
+//  LeavensteeKit
+//
+//  Created by Steven Lee on 1/3/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeavensteeKit.
+FOUNDATION_EXPORT double LeavensteeKitVersionNumber;
+
+//! Project version string for LeavensteeKit.
+FOUNDATION_EXPORT const unsigned char LeavensteeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeavensteeKit/PublicHeader.h>
+
+
